4b37712c00571f3bf6ab15f77a6eb16f0d7acb68
domeneack <112799387+domeneack@users.noreply.github.com>
2023-04-22 13:41:13 -0700

    added weather.css, created form for weather search engine,

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Hello Everyone!
-        </h1>
+        
        <Weather />
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Weather from "./Weather";
+import "./Weather.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Weather />
   </React.StrictMode>
 );
 
