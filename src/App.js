ba41a966a2f8f579962621c2f483b27524ce3256
domeneack <112799387+domeneack@users.noreply.github.com>
2023-04-25 10:30:16 -0700

    Added bootstrap, github link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-
+import React from "react";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./Weather.css";
-
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <Weather />
-        
-      </header>
+      <Weather />
+      <footer className="footer">
+        This project was coded by{" "}
+        <a href="https://tangerine-moonbeam-52db1f.netlify.app/">
+          Domeneack Foster
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/domeneack/weather-react">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
